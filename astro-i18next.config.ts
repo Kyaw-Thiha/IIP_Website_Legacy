0edf742c73d36d0ee72304857d5ce535a03e6e4d
Kyaw-Thiha <bookkay.business@gmail.com>
2022-08-22 15:50:06 +0630

    Initializing Repository

---
+++ astro-i18next.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+
+export default {
+	defaultLanguage: "en",
+	supportedLanguages: ["en", "my"],
+	i18next: {
+		// debug is convenient during development to check for missing keys
+		debug: true,
+		initImmediate: false,
+		backend: {
+			loadPath: "./src/locales/{{lng}}.json",
+		},
+	},
+	i18nextPlugins: { fsBackend: "i18next-localstorage-backend" },
+};

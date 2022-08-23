b279b95782ff434a5c68365a79ca40047e5e04e5
Kyaw-Thiha <bookkay.business@gmail.com>
2022-08-23 12:35:30 +0630

    Restarted astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,4 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import vue from "@astrojs/vue";
-import sitemap from "@astrojs/sitemap";
-import astroI18next from "astro-i18next";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-	experimental: {
-		integrations: true,
-	},
-	integrations: [tailwind(), vue(), sitemap(), astroI18next()],
-});
+export default defineConfig({});

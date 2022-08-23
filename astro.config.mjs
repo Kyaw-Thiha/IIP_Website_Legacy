bdbc25798a40c4cf588e48c139455eeb77bb5ca3
Kyaw-Thiha <bookkay.business@gmail.com>
2022-08-23 13:35:28 +0630

    Add and configured astro-i18next

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+	experimental: {
+		integrations: true,
+	},
+	integrations: [tailwind(), sitemap(), astroI18next()],
 });

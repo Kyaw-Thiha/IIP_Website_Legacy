476988ed5acfa4b1e48180b2d807a43e7254f61f
Kyaw-Thiha <bookkay.business@gmail.com>
2022-08-24 23:49:47 +0630

    Updated astroi18next

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
 	defaultLanguage: "en",
-	supportedLanguages: ["en", "fr"],
+	supportedLanguages: ["en", "my"],
 	i18next: {
 		// debug is convenient during development to check for missing keys
-		debug: true,
+		//debug: true,
 		initImmediate: false,
 		backend: {
 			loadPath: "./src/locales/{{lng}}/{{ns}}.json",
